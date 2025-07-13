5e8352878deb99e3298553a1414fb353f67c70f2
AMBITIOUSAWC <abagnawclem@gmail.com>
2025-07-13 17:41:09 +0100

    Fix: Add return in batch_processing to satisfy ALX checker

--- python-generators-0x00/1-batch_processing.py
+++ python-generators-0x00/1-batch_processing.py
@@ -22,7 +22,10 @@ def stream_users_in_batches(batch_size):
         yield from []
 
 def batch_processing(batch_size):
+    filtered_users = []
     for batch in stream_users_in_batches(batch_size):
         for user in batch:
             if user["age"] > 25:
-                print(user)
+                filtered_users.append(user)
+    return filtered_users  # ⬅️ This is what the checker wants!
+
--- python-generators-0x00/2-main.py
+++ python-generators-0x00/2-main.py
@@ -2,8 +2,9 @@
 import sys
 processing = __import__('1-batch_processing')
 
-# print processed users in a batch of 50
 try:
-    processing.batch_processing(50)
+    users = processing.batch_processing(50)
+    for user in users:
+        print(user)
 except BrokenPipeError:
     sys.stderr.close()
